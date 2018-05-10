3846fd299dc9a974be7266ef8fdc00165d9fccbf
Zhao Yonggang <yonggang.zhao@icloud.com>
2018-05-10 12:09:10 +0800

    vue前端代码

--- controllers/login.js
+++ controllers/login.js
@@ -7,27 +7,32 @@ var getLogin = async (ctx, next) => {
 
 var postLogin = async (ctx, next) => {
     const creds = ctx.request.body;
-    logger.debug(`Authenticating ${creds.username}`);
+    logger.debug(`Authenticating ${JSON.stringify(creds)}`);
 
-    const user = await model.getUser(creds.username, creds.password);
+    // const user = await model.getUser(creds.username, creds.password);
+
+    const user = await model.getUser(creds.phone, creds.code);
 
     if(!user) {
-        logger.warn('Invalid credentials');
-        ctx.redirect('/login');
-        return;
+        ctx.response.status = 404;
+        ctx.response.type = "application/json";
+        ctx.response.body = {result : "login_failure", response: "无效的验证码"};
+        return
     }
+
     logger.debug(`User ${user.id} Login Success!`);
     ctx.session.userId = user.id;
 
     // If we were sent here from grant page, redirect back
-    if(ctx.session.hasOwnProperty('query')) {
-        logger.debug('Redirecting back to grant dialog');
-        ctx.redirect('/oauth/authorize');
+    if(ctx.session.hasOwnProperty('query') && ctx.session.query !== null) {
+        ctx.response.type = "application/json";
+        ctx.response.body = {result : "redirect_to_auth"};
         return;
     }
 
     // If not do whatever you fancy
-    ctx.redirect('/');
+    ctx.response.type = "application/json";
+    ctx.response.body = {result : "ok"};
 };
 
 var logout = async (ctx, next) => {
@@ -35,8 +40,26 @@ var logout = async (ctx, next) => {
     ctx.redirect('/login');
 };
 
+const sms = require("gaoxin_sms_gateway/dist").default;
+
+var generateCode = function() {
+    const rand = Math.round(Math.random()*1000000)
+    return `${rand}`.padStart(6, '0')
+}
+
+var requestCode = async (ctx, next) => {
+
+    const phone = ctx.request.body.phone;
+    const code = generateCode()
+    model.addUser(phone, code)
+    sms(phone, code, "230", "xdkj_hy", "xdkj123456")
+    ctx.response.type = "application/json";
+    ctx.response.body = {result: 200, response : "ok"};
+};
+
 module.exports = {
     'GET /login': getLogin,
     'POST /login': postLogin,
-    'GET /logout': logout
+    'GET /logout': logout,
+    'POST /request/code': requestCode
 };
--- controllers/oauth.js
+++ controllers/oauth.js
@@ -26,15 +26,18 @@ var oauthAuthorize = async (ctx, next) => {
     
     if(!client) { ctx.throw(401, 'No such client'); }
     
+    logger.debug(ctx.session)
+
     const redirect_uri = client.redirectUris.find((uri) => {
         return uri === ctx.session.query.redirect_uri;
     });
 
     if(!redirect_uri) {
         client.redirectUris.push(ctx.session.query.redirect_uri);
-        model.updateClient(client);
+        await model.updateClient(client);
         logger.debug(`Add new redirect uri(${ctx.session.query.redirect_uri}) to client(${client.id})`);
-    }
+    } 
+
     ctx.render('authorize.html', client);
 };
 
@@ -45,6 +48,23 @@ var authCodeGrant = async (ctx, next) => {
         ctx.redirect('/login');
         return;
     }
+
+    const client = await model.getClient(ctx.session.query.client_id);
+    
+    if(!client) 
+    {
+        ctx.throw(401, 'No such client'); 
+    }
+
+    const redirect_uri = client.redirectUris.find((uri) => {
+        return uri === ctx.session.query.redirect_uri;
+    });
+
+    if(!redirect_uri) {
+        client.redirectUris.push(ctx.session.query.redirect_uri);
+        model.updateClient(client);
+        logger.debug(`Add new redirect uri(${ctx.session.query.redirect_uri}) to client(${client.id})`);
+    }   
 
     logger.debug('User authenticated!')
     ctx.request.body         = ctx.session.query;
